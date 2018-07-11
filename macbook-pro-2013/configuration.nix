4ab428f10b26e3a391cc9b0917b490c7f68e0e9c
Zhen-hao <zhenhaolee@gmail.com>
2018-07-11 13:20:10 +0200

    bluetooth working

--- macbook-pro-2013/configuration.nix
+++ macbook-pro-2013/configuration.nix
@@ -152,6 +152,27 @@
   };
 
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.extraConfig = "
+[General]
+ControllerMode = bredr
+Disable=Socket
+Disable=Headset
+Enable=Media,Source,Sink,Gateway
+AutoConnect=true
+load-module module-switch-on-connect
+
+  "; # Disable=Socket
+
+  # hardware.pulseaudio.configFile = pkgs.writeText "default.pa" ''
+    # load-module module-bluetooth-policy
+    # load-module module-bluetooth-discover
+    ## module fails to load with
+    ##   module-bluez5-device.c: Failed to get device path from module arguments
+    ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
+    # load-module module-bluez5-device
+    # load-module module-bluez5-discover
+ # '';
+
 
   # Light
   services.redshift.enable = true;
@@ -188,7 +209,7 @@
 	{ isNormalUser = true;
 	  home = "/home/zhenhao";
 	  description = "Zhenhao Li";
-	  extraGroups = [ "wheel" "networkmanager" ];
+	  extraGroups = [ "wheel" "networkmanager" "audio"];
 	};
 
   # This value determines the NixOS release with which your system is to be
