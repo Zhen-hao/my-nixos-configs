d36c19c3c1e463dd4a62b1cebdc53b9d053ee467
Zhen-hao <zhenhaolee@gmail.com>
2018-07-13 14:49:44 +0200

    added fonts and input methods, printing as well

--- macbook-pro-2013/configuration.nix
+++ macbook-pro-2013/configuration.nix
@@ -26,6 +26,10 @@
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enabled = "fcitx";
+      ibus.engines = with pkgs.fcitx-engines; [ chewing ];
+    };
   };
 
   # Set your time zone.
@@ -50,6 +54,88 @@
   };
 
 
+  fonts = {
+    enableCoreFonts = true;
+    enableFontDir = true;
+    enableGhostscriptFonts = false;
+
+    fonts = with pkgs; [
+      symbola
+      font-awesome-ttf
+      libertine
+      # code2000
+      # code2001
+      # code2002
+      terminus_font
+      ubuntu_font_family
+      liberation_ttf
+      freefont_ttf
+      source-code-pro
+      (let inconsolata-lgc  = let version = "1.2.0";
+           in with pkgs; stdenv.mkDerivation rec {
+             name = "inconsolata-lgc-${version}";
+             src = fetchurl {
+               url = "https://github.com/MihailJP/Inconsolata-LGC/releases/download/LGC-1.2.0/InconsolataLGC-OT-1.2.0.tar.xz";
+               sha256 = "0rw8i481sdqi0pspbvyd2f86k0vlrb6mbi94jmsl1kms18c18p66";
+             };
+             dontBuild = true;
+             installPhase = let
+               fonts_dir = "$out/share/fonts/opentype";
+               doc_dir = "$out/share/doc/${name}";
+             in ''
+               mkdir -pv "${fonts_dir}"
+               mkdir -pv "${doc_dir}"
+               cp -v *.otf "${fonts_dir}"
+               cp -v {README,LICENSE,ChangeLog} "${doc_dir}"
+             '';
+             meta = with stdenv.lib; {
+               homepage = http://www.levien.com/type/myfonts/inconsolata.html;
+               description = "A monospace font for both screen and print, LGC extension";
+               license = licenses.ofl;
+               platforms = platforms.all;
+             };
+           }; in inconsolata-lgc)
+      inconsolata
+      vistafonts
+      dejavu_fonts
+      freefont_ttf
+      unifont
+      cm_unicode
+      ipafont
+      baekmuk-ttf
+      source-han-sans-japanese
+      source-han-sans-korean
+      source-han-sans-simplified-chinese
+      source-han-sans-traditional-chinese
+      source-sans-pro
+      source-serif-pro
+      fira
+      fira-code
+      fira-mono
+      hasklig
+    ];
+    fontconfig = {
+      enable = true;
+      dpi = 200;
+      antialias = true;
+      hinting = {
+        autohint = false;
+        enable = true;
+        #style = "slight";
+      };
+      subpixel.lcdfilter = "default";
+      ultimate = {
+        enable = true; # see also: rendering, substitutions
+      };
+      defaultFonts = {
+        serif = [ "Source Sans Pro" "DejaVu Serif" "IPAMincho" "Beakmuk Batang" ];
+        sansSerif = [ "Source Serif Pro" "DejaVu Sans" "IPAGothic" "Beakmuk Dotum" ];
+        monospace = [ "Inconsolata LGC" "DejaVu Sans Mono" "IPAGothic" "Beakmuk Dotum" ];
+      };
+    };
+  };
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs;
@@ -68,8 +154,8 @@
   in
   [
     # brosers
-    pkgs.firefoxWrapper
-    pkgs.chromium
+    firefoxWrapper
+    chromium
 
     wget # download tool
 
@@ -108,6 +194,7 @@
     lshw # Provide detailed information on the hardware configuration of the machine
     hwinfo # Hardware detection tool from openSUSE
     sysfsutils # These are a set of utilites built upon sysfs, a new virtual filesystem in Linux kernel versions 2.5+ that exposes a system's device tree.
+    nmap #port scanning
 
     strongswan #P2S VPN
     networkmanager_strongswan
@@ -139,8 +226,15 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
+  # VPN
+  services.strongswan.enable = true;
+  services.strongswan.secrets = [ "/run/keys/ipsec.secret" ];
+
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.gutenprint pkgs.gutenprintBin pkgs.cups-bjnp pkgs.foomatic-filters ];
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
 
   # Enable sound.
   sound.enable = true;
